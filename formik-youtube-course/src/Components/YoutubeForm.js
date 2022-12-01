83947034af0dfc69650e622f551ce5752dd370b9
mb-awan <bilalofficialmail123@gmail.com>
2022-12-01 12:13:42 +0500

    Form Validation Function in formik

--- formik-youtube-course/src/Components/YoutubeForm.js
+++ formik-youtube-course/src/Components/YoutubeForm.js
@@ -2,17 +2,37 @@ import styles from '../App.module.css';
 import { useFormik} from "formik";
 
 export const YoutubeForm = props => {
-    const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            channel: ''
-        },
-        onSubmit: values => console.log('form data:', values),
+    const initialValues =  {
+        name: '',
+        email: '',
+        channel: ''
+    };
 
+    const validate = values => {
+        /*
+        There are Three important requirements for validate function of formik that:
+            1. It should always return an object.
+            2. The properties in returned object must be same as the properties in our values input object.
+            3. Each property value in returned object must be of type string.
+        */
+        const error = {};
+        if(!values.name) error.name = "name is required";
+        if(!values.email)
+            error.email = "email is required";
+        else if(!values.email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/))
+            error.email = "invalid email"
+        if(!values.channel) error.channel = "channel is required";
+        return error;
+    }
+
+    const onSubmit = values => console.log('form data:', values);
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate,
     });
 
-    // console.log(formik.values);
+    console.log(formik.errors);
     return (
         <form onSubmit={formik.handleSubmit}>
             <div className={styles['form-control']}>
