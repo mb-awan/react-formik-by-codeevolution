a62d75a5590d1af7d05025e62ea8ffcf3c6fbe31
mb-awan <bilalofficialmail123@gmail.com>
2022-12-01 20:08:45 +0500

    Fields Revisited- as prop and rendor props pattern used in comments and address field respectively

--- formik-youtube-course/src/Components/YoutubeForm.js
+++ formik-youtube-course/src/Components/YoutubeForm.js
@@ -5,14 +5,18 @@ import * as Yup from 'yup';
 const validationSchema = Yup.object({
     name: Yup.string().required("name is required"),
     email: Yup.string().email("Invalid email").required("email is required"),
-    channel: Yup.string().required("channel is required")
+    channel: Yup.string().required("channel is required"),
+    comment: Yup.string().required('comment is required'),
+    address: Yup.string().required('Address is required')
 });
 
 export const YoutubeForm = props => {
     const initialValues =  {
         name: '',
         email: '',
-        channel: ''
+        channel: '',
+        comment: '',
+        address: ''
     };
 
     const onSubmit = values => console.log('form data:', values);
@@ -21,8 +25,7 @@ export const YoutubeForm = props => {
         <Formik
             initialValues={initialValues}
             onSubmit={onSubmit}
-            validationSchema={validationSchema}
-        >
+            validationSchema={validationSchema}>
             <Form>
                 <div className={styles['form-control']}>
                     <label htmlFor='name'>Name</label>
@@ -52,6 +55,32 @@ export const YoutubeForm = props => {
                         name='channel'
                     />
                     <ErrorMessage name='channel'/>
+                </div>
+
+                <div className={styles['form-control']}>
+                    <label id='comment'>Comment</label>
+                    <Field
+                        as='textarea'
+                        id='comment'
+                        name='comment'
+                    />
+                    <ErrorMessage name='comment'/>
+                </div>
+
+                {/* Props Render Method:  We can also create our custom input element using Field Component of formik*/}
+                <div className={styles['form-control']}>
+                    <label>Address</label>
+                    <Field name='address'>
+                        { props => {
+                            const { field, form, meta} = props;
+                            return (
+                                <>
+                                    <input id='address' type='text' {...field}/>
+                                    {meta.touched && meta.error && <div>{meta.error}</div>}
+                                </>
+                            )
+                        }}
+                    </Field>
                 </div>
 
                 <div className={styles['form-control']}>
