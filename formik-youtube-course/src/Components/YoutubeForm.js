4aabf8b5e9523174d28c17676fa1b2f991863f18
mb-awan <bilalofficialmail123@gmail.com>
2022-12-05 19:52:23 +0500

    026-menually triggering the validation- using validateField, validateForm with setFieldTouched and setTouched

--- formik-youtube-course/src/Components/YoutubeForm.js
+++ formik-youtube-course/src/Components/YoutubeForm.js
@@ -42,116 +42,138 @@ export const YoutubeForm = props => {
         <Formik
             initialValues={initialValues}
             onSubmit={onSubmit}
-            validationSchema={validationSchema}>
-            <Form>
-                <div className={styles['form-control']}>
-                    <label htmlFor='name'>Name</label>
-                    <Field
-                        type='text'
-                        id='name'
-                        name='name'
-                    />
-                    <ErrorMessage name='name' component={ErrorMessageContainer}/>
-                </div>
+            validationSchema={validationSchema}
+        >{
+            formik => {
+                console.log(formik);
+                return (
+                    <Form>
+                        <div className={styles['form-control']}>
+                            <label htmlFor='name'>Name</label>
+                            <Field
+                                type='text'
+                                id='name'
+                                name='name'
+                            />
+                            <ErrorMessage name='name' component={ErrorMessageContainer}/>
+                        </div>
 
-                <div className={styles['form-control']}>
-                    <label htmlFor='email'>Email</label>
-                    <Field
-                        type='email'
-                        id='email'
-                        name='email'
-                    />
-                    {/*Error Message Wrapper using props render pattern*/}
-                    <ErrorMessage name='email'>
-                        {props => {
-                            return <div className={styles.error}>{props}</div>
-                        }}
-                    </ErrorMessage>
-                </div>
+                        <div className={styles['form-control']}>
+                            <label htmlFor='email'>Email</label>
+                            <Field
+                                type='email'
+                                id='email'
+                                name='email'
+                            />
+                            {/*Error Message Wrapper using props render pattern*/}
+                            <ErrorMessage name='email'>
+                                {props => {
+                                    return <div className={styles.error}>{props}</div>
+                                }}
+                            </ErrorMessage>
+                        </div>
 
-                <div className={styles['form-control']}>
-                    <label htmlFor='channel'>Channel</label>
-                    <Field
-                        type='text'
-                        id='channel'
-                        name='channel'
-                    />
-                    <ErrorMessage name='channel' component={ErrorMessageContainer}/>
-                </div>
+                        <div className={styles['form-control']}>
+                            <label htmlFor='channel'>Channel</label>
+                            <Field
+                                type='text'
+                                id='channel'
+                                name='channel'
+                            />
+                            <ErrorMessage name='channel' component={ErrorMessageContainer}/>
+                        </div>
 
-                <div className={styles['form-control']}>
-                    <label id='comment'>Comment</label>
-                    <Field
-                        as='textarea'
-                        id='comment'
-                        name='comment'
-                        validate={commentValidation}
-                    />
-                    <ErrorMessage name='comment' component={ErrorMessageContainer}/>
-                </div>
+                        <div className={styles['form-control']}>
+                            <label id='comment'>Comment</label>
+                            <Field
+                                as='textarea'
+                                id='comment'
+                                name='comment'
+                                validate={commentValidation}
+                            />
+                            <ErrorMessage name='comment' component={ErrorMessageContainer}/>
+                        </div>
 
-                {/* Props Render Method:  We can also create our custom input element using Field Component of formik*/}
-                <div className={styles['form-control']}>
-                    <label>Address</label>
-                    <Field name='address'>
-                        { props => {
-                            const {field, meta} = props; // one more property form also exist in props objects in Field component props
-                            return (
-                                <>
-                                    <input id='address' type='text' {...field}/>
-                                    {meta.touched && meta.error && <div className={styles.error}>{meta.error}</div>}
-                                </>
-                            )
-                        }}
-                    </Field>
-                </div>
-
-                <div className={styles['form-control']}>
-                    <label htmlFor='fb'>Facebook: </label>
-                    <Field
-                        id='fb'
-                        name='social.fb'
-                    />
-                </div>
-
-                <div className={styles['form-control']}>
-                    <label htmlFor='insta'>Instagram: </label>
-                    <Field
-                        id='insta'
-                        name='social.insta'
-                    />
-                </div>
-
-                <div className={styles['form-control']}>
-                    <label>Phone Numbers:</label>
-                    <Field name='phoneNumbers[0]' placeholder='primary'></Field>
-                    <Field name='phoneNumbers[1]' placeholder='secondary'></Field>
-                </div>
-
-                <div className={styles['form-control']}>
-                    <label>Dynamic Phone Numbers Array</label>
-                    <FieldArray name='phNumbers'>
-                        {
-                            fieldArrayProps => {
-                                const {push, remove, form} = fieldArrayProps;
-                                const {phNumbers} = form.values;
-                                return phNumbers.map((phNumber, index) => (
-                                        <div key={index}>
-                                            <Field name={`phNumbers[${index}]`}></Field>
-                                            { !!index && <button type='button' onClick={()=> {remove(index)}}>-</button>}
-                                            <button type='button' onClick={()=> { push('')}}>+</button>
-                                        </div>
+                        {/* Props Render Method:  We can also create our custom input element using Field Component of formik*/}
+                        <div className={styles['form-control']}>
+                            <label>Address</label>
+                            <Field name='address'>
+                                { props => {
+                                    const {field, meta} = props; // one more property form also exist in props objects in Field component props
+                                    return (
+                                        <>
+                                            <input id='address' type='text' {...field}/>
+                                            {meta.touched && meta.error && <div className={styles.error}>{meta.error}</div>}
+                                        </>
                                     )
-                                )
-                            }
-                        }
-                    </FieldArray>
-                </div>
+                                }}
+                            </Field>
+                        </div>
 
-                <div className={styles['form-control']}>
-                    <button type='submit'>Submit</button>
-                </div>
-            </Form>
+                        <div className={styles['form-control']}>
+                            <label htmlFor='fb'>Facebook: </label>
+                            <Field
+                                id='fb'
+                                name='social.fb'
+                            />
+                        </div>
+
+                        <div className={styles['form-control']}>
+                            <label htmlFor='insta'>Instagram: </label>
+                            <Field
+                                id='insta'
+                                name='social.insta'
+                            />
+                        </div>
+
+                        <div className={styles['form-control']}>
+                            <label>Phone Numbers:</label>
+                            <Field name='phoneNumbers[0]' placeholder='primary'></Field>
+                            <Field name='phoneNumbers[1]' placeholder='secondary'></Field>
+                        </div>
+
+                        <div className={styles['form-control']}>
+                            <label>Dynamic Phone Numbers Array</label>
+                            <FieldArray name='phNumbers'>
+                                {
+                                    fieldArrayProps => {
+                                        const {push, remove, form} = fieldArrayProps;
+                                        const {phNumbers} = form.values;
+                                        return phNumbers.map((phNumber, index) => (
+                                                <div key={index}>
+                                                    <Field name={`phNumbers[${index}]`}></Field>
+                                                    { !!index && <button type='button' onClick={()=> {remove(index)}}>-</button>}
+                                                    <button type='button' onClick={()=> { push('')}}>+</button>
+                                                </div>
+                                            )
+                                        )
+                                    }
+                                }
+                            </FieldArray>
+                        </div>
+
+                        <button type='button' onClick={()=> {
+                            formik.validateField('comment');
+                            formik.setFieldTouched('comment');
+                        }}>Validate Comments</button>
+
+                        <button type='button' onClick={()=> {
+                            formik.validateForm();
+                            formik.setTouched({
+                                name: true,
+                                email: true,
+                                channel: true,
+                                comment: true
+                            });
+                        }}>Validate All</button>
+
+                        <div className={styles['form-control']}>
+                            <button type='submit'>Submit</button>
+                        </div>
+                    </Form>
+                )
+            }
+        }
         </Formik>
     )
 }
