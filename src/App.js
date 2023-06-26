01e13de00abeeb21197263ed51b6e927d0adca78
Muhammad Bilal <bilalofficialmail123@gmail.com>
2023-06-26 09:47:56 +0500

    fixed the structure

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
-import {Expenses} from "./components/Expenses/Expenses";
-import {NewExpense} from "./components/NewExpense/NewExpense";
-import {useState} from "react";
+import './App.css';
+// import RegistrationForm from "./Components/RegistrationForm";
+// import CourseEnrolmentForm from "./Components/CourseEnrolmentForm";
+// import FormikContainer from "./Components/FormikContainer";
+import LoginForm from "./Components/LoginForm";
+import {ChakraProvider} from '@chakra-ui/react'
 
-const DUMMY_EXPENSES = [
-    {
-        id: 'e1',
-        title: 'Toilet Paper',
-        amount: 94.12,
-        date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-        id: 'e3',
-        title: 'Car Insurance',
-        amount: 294.67,
-        date: new Date(2021, 2, 28),
-    },
-    {
-        id: 'e4',
-        title: 'New Desk (Wooden)',
-        amount: 450,
-        date: new Date(2021, 5, 12),
-    },
-];
-
-function App() {
-    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-
-    const addExpenseHandler = expense => {
-        setExpenses(prevExpense => {
-            return [expense,...prevExpense];
-        })
-    }
-
+const App = () => {
     return (
-        <div>
-            <NewExpense onAddExpense = {addExpenseHandler}/>
-            <Expenses items={expenses}/>
-        </div>
+
+        <ChakraProvider>
+            <div className='App'>
+                {/*<FormikContainer/>*/}
+                <LoginForm/>
+                {/*<RegistrationForm/>*/}
+                {/*<CourseEnrolmentForm/>*/}
+            </div>
+        </ChakraProvider>
     );
 }
 
